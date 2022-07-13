20e5e66d3e29d9aa12a3bdecccedccb904ceb055
sailormoor <vasilmaru@yandex.ru>
2022-07-13 20:50:35 +0300

    files added

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -1,6 +1,10 @@
 package ru.netology.javacore;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -8,14 +12,49 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class TodoServer {
-    //...
+
+    private int port;
+    private Todos todos;
 
     public TodoServer(int port, Todos todos) {
-        //...
+        this.port = port;
+        this.todos = todos;
     }
 
     public void start() throws IOException {
         System.out.println("Starting server at " + port + "...");
-        //...
+        JSONParser parser = new JSONParser();
+        try (ServerSocket serverSocket = new ServerSocket(port);) {
+
+            while (true) {
+                try (
+                        Socket clientSocket = serverSocket.accept();
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                ) {
+                    final String clientTaskJson = in.readLine();
+
+                    //Parser
+
+                    Object obj = parser.parse(clientTaskJson);
+                    JSONObject jsonObject = (JSONObject) obj;
+                    String typeParse = (String) jsonObject.get("type");
+                    String taskParse = (String) jsonObject.get("task");
+
+                    if (typeParse.equals("ADD")) {
+                        todos.addTask(taskParse);
+                    } else {
+                        if (typeParse.equals("REMOVE")) {
+                            todos.removeTask(taskParse);
+                        }
+                    }
+                    //sent to client
+                    out.println(String.format(todos.getAllTasks()));
+                }
+            }
+        } catch (IOException | ParseException e) {
+            System.out.println("WRONG!");
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/ru/netology/javacore/TodosTests.java
+++ src/test/java/ru/netology/javacore/TodosTests.java
@@ -1,6 +1,65 @@
 package ru.netology.javacore;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TodosTests {
 
-    // ваши тесты для класса Todos
+    Todos sut = new Todos();
+
+    @org.junit.jupiter.api.Test
+    public void addTaskTest() {
+        // given:
+        String task = "D";
+        ArrayList<String> list1 = new ArrayList<>();
+        list1.add(task);
+
+        // when:
+        sut.addTask(task);
+
+        // then:
+        assertTrue(list1.equals(sut.getListTasks()));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void removeTaskTest() {
+        // given:
+        String task = "D";
+        ArrayList<String> list1 = new ArrayList<>();
+        list1.remove(task);
+
+        // when:
+        sut.removeTask(task);
+
+        // then:
+        assertTrue(list1.equals(sut.getListTasks()));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void getAllTasksTest() {
+        // given:
+        ArrayList<String> list1 = new ArrayList<>();
+        String task = "D";
+        String task1 = "A";
+        list1.add(task);
+        list1.add(task1);
+
+        Collections.sort(list1);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list1.size(); i++) {
+            sb.append(list1.get(i) + " ");
+        }
+        String result = sb.toString();
+
+        // when:
+        sut.addTask("D");
+        sut.addTask("A");
+        sut.getAllTasks();
+
+        // then:
+        assertTrue(result.equals(sut.getAllTasks()));
+    }
 }
